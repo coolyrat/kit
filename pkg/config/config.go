205aa594b7e4119022885cacae69ec2ddf8bb8a7
luischan <v.luis.chen@welab.co>
2022-05-06 18:50:58 +0800

    refactor the watch logic

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"fmt"
+
 	"github.com/coolyrat/kit/pkg/config/configcenter"
 	"github.com/knadh/koanf"
 	"github.com/mitchellh/mapstructure"
@@ -13,8 +15,45 @@ type config struct {
 	configCenter configcenter.ConfigCenter
 }
 
-func RegisterWatcher(dataID string, cb func()) {
-	conf.configCenter.RegisterWatcher(dataID, cb)
+type File interface {
+	DataID() string
+	ConfigPath() string
+}
+
+type Watcher interface {
+	File
+	CallbackFn() func()
+}
+
+func RegisterWatcher(w Watcher) {
+	load := func() {
+		err := Unmarshal(w.ConfigPath(), w)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	cb := w.CallbackFn()
+	if cb == nil {
+		cb = load
+	}
+
+	load()
+	conf.configCenter.RegisterWatcher(w.DataID(), cb)
+}
+
+func InitConfigFile(f File) error {
+	if err := Unmarshal(f.ConfigPath(), f); err != nil {
+		return fmt.Errorf("init config file %s failed, %w", f.DataID(), err)
+	}
+
+	return nil
+}
+
+func MustInitConfigFile(f File) {
+	if err := InitConfigFile(f); err != nil {
+		panic(err)
+	}
 }
 
 func Unmarshal(path string, v interface{}) error {
